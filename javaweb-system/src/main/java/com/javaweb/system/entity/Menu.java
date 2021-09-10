4b335b162c4b25232821c85b0cbef28496410f20
guyun <1186587430@qq.com>
2021-09-10 10:13:41 +0800

    first commit

---
+++ javaweb-system/src/main/java/com/javaweb/system/entity/Menu.java
@@ -0,0 +1,109 @@
+// +----------------------------------------------------------------------
+// | JavaWeb混编版框架 [ JavaWeb ]
+// +----------------------------------------------------------------------
+// | 版权所有 2019~2020 南京JavaWeb研发中心
+// +----------------------------------------------------------------------
+// | 官方网站: http://www.javaweb.vip/
+// +----------------------------------------------------------------------
+// | 作者: 鲲鹏 <1175401194@qq.com>
+// +----------------------------------------------------------------------
+
+package com.javaweb.system.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javaweb.common.common.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+/**
+ * <p>
+ * 菜单
+ * </p>
+ *
+ * @author 鲲鹏
+ * @since 2020-05-07
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("sys_menu")
+public class Menu extends BaseEntity {
+
+    /**
+     * 菜单名称
+     */
+    private String name;
+
+    /**
+     * 图标
+     */
+    private String icon;
+
+    /**
+     * URL地址
+     */
+    private String url;
+
+    /**
+     * 参数
+     */
+    private String param;
+
+    /**
+     * 上级ID
+     */
+    private Integer pid;
+
+    /**
+     * 类型：1模块 2导航 3菜单 4节点
+     */
+    private Integer type;
+
+    /**
+     * 权限标识
+     */
+    private String permission;
+
+    /**
+     * 是否显示：1显示 2不显示
+     */
+    private Integer status;
+
+    /**
+     * 是否公共：1是 2否
+     */
+    private Integer isPublic;
+
+    /**
+     * 菜单备注
+     */
+    private String note;
+
+    /**
+     * 显示顺序
+     */
+    private Integer sort;
+
+    /**
+     * 菜单节点
+     */
+    @TableField(exist = false)
+    private String funcIds;
+
+    /**
+     * 子级菜单
+     */
+    @TableField(exist = false)
+    private List<Menu> children;
+
+}
